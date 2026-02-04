3957d2f9d278e5eb7ef3d3a4873624b25d465b6f
Ovi Demetrian Jr <ovi@distinctivequality.com>
2026-02-04 11:32:53 -0700

    Adds automated navigation active state

--- functions.js
+++ functions.js
@@ -1,3 +1,19 @@
+/* Menu item active state */
+
+var currentURL = window.location.href;
+var navItems = document.querySelectorAll('a');
+
+function matchExact(r, str) {
+  var match = str.match(r);
+  return match && str === match[0];
+}
+
+for (var i = 1; i < navItems.length; ++i) {
+  if (matchExact(currentURL.match(navItems[i].href), currentURL)) {
+    navItems[i].classList.add("on");
+  }
+}
+
 /* Mobile navigation */
 
 document.querySelector(".menu").onclick = function() {
